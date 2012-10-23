8af3a2ae0e34d5d5601753b5156dbb5f500116c9
U-peke2-7\peke2 <peke2@peke2-7.(none)>
2012-10-24 02:30:59 +0900

    xpathによるデータの取得が出来た。

--- test_http.rb
+++ test_http.rb
@@ -11,7 +11,7 @@ def readHTTP(url)
 	return	result
 end
 
-#result = readHTTP("http://192.168.56.102/peke2/project/cake/memo/app/webroot/test.php?case=models%2Fmemo.test.php&app=true")
+##result = readHTTP("http://192.168.56.102/peke2/project/cake/memo/app/webroot/test.php?case=models%2Fmemo.test.php&app=true")
 #result = readHTTP("http://192.168.56.102/peke2/project/cake/memo/app/webroot/test.php?case=controllers%2Fmemos_controller.test.php&app=true")
 #doc = REXML::Document.new(result)
 #print result
@@ -20,16 +20,25 @@ end
 #	p element
 #}
 
-doc = Nokogiri::HTML::Document.new()
-top_node = doc.parse(open("http://192.168.56.102/peke2/project/cake/memo/app/webroot/test.php?case=controllers%2Fmemos_controller.test.php&app=true"))
-#node = top_node.root()
-top_node.each do |node|
-	print node
-end
-
-#doc.xpath('//hs/a').each do |link|
-#	p link.content
+#doc = Nokogiri::HTML::Document.new()
+#top_node = doc.parse(open("http://192.168.56.102/peke2/project/cake/memo/app/webroot/test.php?case=controllers%2Fmemos_controller.test.php&app=true"))
+##node = top_node.root()
+#top_node.each do |node|
+#	print node
 #end
+
+doc = Nokogiri::HTML(readHTTP("http://192.168.56.102/peke2/project/cake/memo/app/webroot/test.php?case=controllers%2Fmemos_controller.test.php&app=true"))
+doc.xpath('//html//body//ul').each do |node|
+	#	テスト時に出力されたテキストを取得
+	if node["class"] == 'tests'
+		print node.content
+		print "\n"
+		#	該当するテキストの次の兄弟ノードがテストの結果らしい
+		next_node = node.next_sibling()
+		print next_node.content
+		print "\n"
+	end
+end
 
 
 #class	MyDoc < Nokogiri::XML::SAX::Document
