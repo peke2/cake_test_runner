31c3f30813b7f104fc5900c6f56090bc4191e53c
U-peke2-7\peke2 <peke2@peke2-7.(none)>
2012-10-29 02:04:21 +0900

    テスト実行から結果出力までの流れが出来た。

--- CakeTestRunner/cake_test_runner.rb
+++ CakeTestRunner/cake_test_runner.rb
@@ -1,10 +1,13 @@
+require "open-uri"
 require "nokogiri"
+require "time"
 
 class	CakeTestRunner
 	TESTCASE = Struct.new(:classname, :methodname, :time, :error_infos)
 	ERRORINFO = Struct.new(:type, :description)
 
 	@testcases
+	@test_date
 
 	def	initialize
 		@testcases = Array.new
@@ -25,20 +28,24 @@ class	CakeTestRunner
 	#	指定されたURLのテストを実行
 	#
 	def	runTestByURL(url)
-		convertResult( readHTTP(url) )
+		result_text = readHTTP(url)
+		date = Time.now
+		retrieveResult(result_text, date) 
 	end
 
 
 	#
 	#	テスト結果のテキストから必要な情報を取得する
 	#
-	def	retrieveResult(result_text)
+	def	retrieveResult(result_text, date)
+		@test_date = convertDateTime(date)
+
 		doc = Nokogiri::HTML(result_text)
 
 		test_case = TESTCASE.new
 
 		#<div  class="test-results"><h2>Individual test case: controllers/memos_controller.test.php</h2>
-		doc.xpath("//div[@class='test-results']").each do |node|
+		doc.xpath("//div[@class='test-results']/h2").each do |node|
 			#とりあえず、最初に見つけた1つのみを取得
 			#実際は単体でテストを起動するので1つしか無い気がする
 			test_case.classname = node.content
@@ -88,32 +95,55 @@ class	CakeTestRunner
 		doc.encoding = "utf-8"
 
 		top_node = Nokogiri::XML::Node.new("testsuite", doc)
-		top_node["errors"] = "1"
-		top_node["failures"] = "1"
-		top_node["tests"] = "2"
+		top_node["errors"] = getErrorCount().to_s
+		top_node["failures"] = getFailureCount().to_s
+		top_node["tests"] = "*"
 		top_node["time"] = "0.056"
-		top_node["timestamp"] = "2012-10-28T19:45:00"
+		top_node["timestamp"] = @test_date
 		doc.add_child(top_node)
 
-		node = Nokogiri::XML::Node.new("testcase", doc)
-		node["classname"] = "DummyClass"
-		node["name"] = "dummyMethod"
-		node["time"] = "0.0345"
-		top_node.add_child(node)
+		#node = Nokogiri::XML::Node.new("testcase", doc)
+		#node["classname"] = "DummyClass"
+		#node["name"] = "dummyMethod"
+		#node["time"] = "0.0345"
+		#top_node.add_child(node)
 
-		error_node = Nokogiri::XML::Node.new("error", doc)
-		error_node["message"] = "ERROR"
-		error_node.content = "Syntax error on DummyFile.php in line 123"
-		node.add_child(error_node)
+		#error_node = Nokogiri::XML::Node.new("error", doc)
+		#error_node["message"] = "ERROR"
+		#error_node.content = "Syntax error on DummyFile.php in line 123"
+		#node.add_child(error_node)
 
-		error_node = Nokogiri::XML::Node.new("failure", doc)
-		error_node["message"] = "FAILURE"
-		error_node.content = "Assertion failed"
-		node.add_child(error_node)
+		#error_node = Nokogiri::XML::Node.new("failure", doc)
+		#error_node["message"] = "FAILURE"
+		#error_node.content = "Assertion failed"
+		#node.add_child(error_node)
 
-		io = File.open("output.xml", "w")
+		@testcases.each do |testcase|
+			node = Nokogiri::XML::Node.new("testcase", doc)
+			node["classname"] = testcase.classname
+			#node["name"]      = testcase.methodname
+			#node["time"]      = testcase.time
+			top_node.add_child(node)
+
+			testcase.error_infos.each do |error_info|
+				error_node = Nokogiri::XML::Node.new(error_info.type, doc)
+				error_node["message"] = error_info.type
+				error_node.content = error_info.description
+				node.add_child(error_node)
+			end
+		end
+
+		io = File.open(filename, "w")
 		doc.write_xml_to(io)
 		io.close()
+	end
+
+
+	#
+	#	日付を出力用に変換
+	#
+	def	convertDateTime(datetime)
+		return	datetime.iso8601
 	end
 
 
--- test/test_cake_test_runner.rb
+++ test/test_cake_test_runner.rb
@@ -1,6 +1,6 @@
 require '../CakeTestRunner/cake_test_runner.rb'
 require 'test/unit'
-
+require 'time'
 
 class TestCakeTestRunner < Test::Unit::TestCase
 	def	setup
@@ -14,10 +14,36 @@ class TestCakeTestRunner < Test::Unit::TestCase
 		io = File.open('test_result.txt', "r")
 		text = io.read()
 		io.close()
-		@cake_test_runner.retrieveResult(text)
+		@cake_test_runner.retrieveResult(text, Time.now)
 
 		assert_equal(1, @cake_test_runner.getErrorCount())
 		assert_equal(2, @cake_test_runner.getFailureCount())
-
 	end
+
+	def	test_outputResult
+		io = File.open('test_result.txt', "r")
+		text = io.read()
+		io.close()
+
+		@cake_test_runner.retrieveResult(text, Time.now)
+		@cake_test_runner.outputResult("test_result.xml")
+	end
+
+
+	def	test_outputResultWithURL
+		@cake_test_runner.runTestByURL("http://192.168.56.102/peke2/project/cake/memo/app/webroot/test.php?case=controllers%2Fmemos_controller.test.php&app=true")
+
+		@cake_test_runner.outputResult("test_result.xml")
+	end
+
+
+	def	test_convertDateTime
+		#datetime = File.mtime("test_result.txt")
+		datetime = Time.mktime(2012,10,28,21,40,05)
+	#	p datetime
+
+		result = @cake_test_runner.convertDateTime(datetime)
+		assert_equal("2012-10-28T21:40:05+09:00", result)
+	end
+
 end
