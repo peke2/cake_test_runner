c12cf19ee6c2ce2ed801093f8248c0dbc087c436
U-peke2-7\peke2 <peke2@peke2-7.(none)>
2012-10-28 18:56:42 +0900

    Nokogiriのxpathによる情報の取得が確認できた。

--- test_http.rb
+++ test_http.rb
@@ -27,18 +27,50 @@ end
 #	print node
 #end
 
-doc = Nokogiri::HTML(readHTTP("http://192.168.56.102/peke2/project/cake/memo/app/webroot/test.php?case=controllers%2Fmemos_controller.test.php&app=true"))
-doc.xpath('//html//body//ul').each do |node|
+url = "http://192.168.56.102/peke2/project/cake/memo/app/webroot/test.php?case=controllers%2Fmemos_controller.test.php&app=true"
+
+result_text = readHTTP(url)
+io = File.open("result.txt", "w")
+io.write(result_text)
+io.close()
+
+doc = Nokogiri::HTML(readHTTP(url))
+doc.xpath("//ul[@class='tests']").each do |node|
 	#	テスト時に出力されたテキストを取得
-	if node["class"] == 'tests'
-		print node.content
-		print "\n"
-		#	該当するテキストの次の兄弟ノードがテストの結果らしい
-		next_node = node.next_sibling()
-		print next_node.content
-		print "\n"
-	end
+	print node.content
+	print "\n"
+	#	該当するテキストの次の兄弟ノードがテストの結果らしい
+	print("-------- [next] --------\n");
+	next_node = node.next_sibling()
+	print next_node.content
+	print "\n"
 end
+
+print("\n\n")
+
+#	エラーメッセージを取り出す
+print("-------- [Errors] --------\n")
+
+doc.xpath("//li[@class='error' or @class='fail']//div[@class='msg']").each do |node|
+	parent_node = node.parent()
+	printf("paren name=[%s]\n", parent_node.node_name())
+	attr = "class"
+	printf("attribute [%s]=[%s]\n", attr, parent_node.get_attribute(attr))
+
+	#	エラーが発生した行
+	print node.content
+	print "\n"
+	#	詳細情報？
+	#	ノードの設定を表示してみたら「兄弟」ではなく「子供」だった…
+	#	構造上、どうみても対象ノードの子供では無いような気がするのだが…
+	print("-------- [next] --------\n");
+#	next_node = node.next_sibling()
+	next_node = node.next_element()
+	p next_node
+	print next_node.content
+	print "\n\n"
+end
+
 
 
 #class	MyDoc < Nokogiri::XML::SAX::Document
