3df3a910ff03b0c8d6abc4d70f92e91e16c0c22f
U-92a-abe\92 <92@92a-abe.(none)>
2012-10-29 15:19:16 +0900

    他ファイルの参照先パスにカレントディレクトリの指定を追加。

--- CakeTestRunner/start_test.rb
+++ CakeTestRunner/start_test.rb
@@ -1,4 +1,4 @@
-require 'cake_test_runner'
+require './cake_test_runner'
 
 cake_test_runner = CakeTestRunner.new
 cake_test_runner.runTestByURL("http://192.168.56.102/peke2/project/cake/memo/app/webroot/test.php?case=controllers%2Fmemos_controller.test.php&app=true")
