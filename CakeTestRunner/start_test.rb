be78ad978feffdd3833731706ed5928e2f80adb8
U-peke2-7\peke2 <peke2@peke2-7.(none)>
2012-11-03 20:49:52 +0900

    Jenkinsで使用するための呼び出しスクリプトにファイル指定処理を組み込んだ。

--- CakeTestRunner/start_test.rb
+++ CakeTestRunner/start_test.rb
@@ -7,8 +7,10 @@ params = "app=true&show_passes=1"
 cake_test_runner = CakeTestRunner.new
 #cake_test_runner.runTestByURL("http://192.168.56.102/peke2/project/cake/memo/app/webroot/test.php?case=controllers%2Fmemos_controller.test.php&app=true")
 
-ARGV.each do |arg|
-	uri = sprintf("%s?case=%s&%s", url, URI.encode(arg,"/"), params )
+targets = cake_test_runner.retrieveTestTarget(ARGV)
+
+targets.each { |target|
+	uri = sprintf("%s?case=%s&%s", url, URI.encode(target,"/"), params )
 	cake_test_runner.runTestByURL(uri)
-end
+}
 cake_test_runner.outputResult("test_result.xml")
